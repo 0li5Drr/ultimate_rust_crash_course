aca81ed239fbe7749ba765837063fe66c2cee9f9
ODuerer VSCTerminal <captainjolt@LAPTOP-759GSQGC.localdomain>
2022-12-01 17:00:30 +0100

    Added an alternative structure to define shots in exerciose g

--- exercise/g_collections_enums/src/main.rs
+++ exercise/g_collections_enums/src/main.rs
@@ -8,6 +8,11 @@
 // - `Hit`, containing the distance from the center (an f64)
 // - `Miss`
 //
+enum Shot {
+    Bullseye,
+    Hit(f64),
+    Miss,
+}
 // You will need to complete 1b as well before you will be able to run this program successfully.
 
 impl Shot {
@@ -18,6 +23,13 @@ impl Shot {
         // - return 2 points if `self` is a `Shot::Hit(x)` where x < 3.0
         // - return 1 point if `self` is a `Shot::Hit(x)` where x >= 3.0
         // - return 0 points if `self` is a Miss
+        return match self {
+            Shot::Bullseye => 5,
+            Shot::Hit(x) => {
+                if x < 3.0 {2} else {1}
+            },
+            Shot::Miss => 0
+        };
     }
 }
 
@@ -34,10 +46,30 @@ fn main() {
     //      - Less than 1.0 -- `Shot::Bullseye`
     //      - Between 1.0 and 5.0 -- `Shot::Hit(value)`
     //      - Greater than 5.0 -- `Shot::Miss`
-
+    for coord inarrow_coords {
+        coord.print_description();
+        let distance = arrow.distance_from_center();
+        if distance < 1.0 {
+            shots.push(Shot::Bullseye)
+        } else if distance < 5.0 {
+            shots.push(Shot::Hit(distance))
+        } else {
+            shots.push(Shot::Miss)
+        }
+        // Alternative Structure:
+        // let shot = match coord.distance_from_center() {
+        //     x if x < 1.0 => Shot::Bullseye,
+        //     x if x < 5.0 => Shot::Hit(x),
+        //     _ =>            Shot::Miss,
+        // };
+        // shots.push(shot)
+    }
 
     let mut total = 0;
     // 3. Finally, loop through each shot in shots and add its points to total
+    for shot in shots {
+        total += shot.points()
+    }
 
     println!("Final point total is: {}", total);
 }
